7378d08f0e9f677caec2e3d91dc2a3d2e28931b1
Jonathan <jonyal2010@live.com.mx>
2026-03-09 12:27:03 -0600

    Structure

---
+++ index.php
@@ -0,0 +1,15 @@
+//Abrir tareas tasks json
+<?php
+$tasks = json_decode(file_get_contents('tasks.json'), true);
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+
+
+</body>
